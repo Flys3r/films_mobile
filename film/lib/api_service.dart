1996044bc7e8999cad57bc4a29d4731397b04f36
Flys3r <118739922+Flys3r@users.noreply.github.com>
2024-05-02 11:00:19 +0200

    Lister les films

---
+++ film/lib/api_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '/models/film.dart';
+
+
+class ApiService {
+  Future<List<Movie>> listfilm() async {
+    final response = await http.get(Uri.parse(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=Ajouter_API'));
+    if (response.statusCode == 200) {
+      final parsed = json.decode(response.body);
+      final List<Movie> movies = List<Movie>.from(
+          parsed['results'].map((json) => Movie.fromJson(json)));
+      return movies;
+    } else {
+      throw Exception('Erreur');
+    }
+  }
+}
+
+
+
